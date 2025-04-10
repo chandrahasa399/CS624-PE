25249756f763453c54ab768f9aed19cafa494952
chandrahasa399 <147586327+chandrahasa399@users.noreply.github.com>
2025-04-10 17:51:16 +0000

    submission for pe01

---
+++ pe01/app/(tabs)/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Chandrahasa Munagala{'\n'}
+        MSCS{'\n'}
+        City University of Seattle
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
